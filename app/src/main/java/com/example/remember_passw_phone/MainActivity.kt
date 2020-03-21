21452f689bb07617ea6f68bdbdfa275d8ba4d14f
cerdolic <melniktk12@gmail.com>
2020-03-21 16:18:26 +0200

    initial commit

---
+++ app/src/main/java/com/example/remember_passw_phone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.remember_passw_phone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
